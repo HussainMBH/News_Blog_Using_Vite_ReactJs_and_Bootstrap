cbeaba9e5bf29d4620383c620d328a148f6615a7
Bahir Hussain <mbahirh624@gmail.com>
2024-02-15 15:32:23 +0530

    Update

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navebar.jsx";
+
+import NewsBoard from "./components/NewsBoard.jsx"
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <NewsBoard />
+
+    </div>
+  )
+}
+
+export default App
